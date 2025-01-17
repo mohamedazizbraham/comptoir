1929a6ce0936948d660a749944ee0cc2b16716ab
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2025-01-17 14:59:54 +0100

    commit de 1er question (modification dans CommandeRepository)

--- src/main/java/comptoirs/dao/CommandeRepository.java
+++ src/main/java/comptoirs/dao/CommandeRepository.java
@@ -8,5 +8,13 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface CommandeRepository extends JpaRepository<Commande, Integer> {
-
+    @Query(
+            // Chaîne de caractères multilignes
+            """
+             SELECT SUM(l.quantite *l.produit.prixUnitaire* (1-l.commande.remise))   
+             FROM Ligne l
+             WHERE l.commande.numero= :numeroCommande
+             
+            """)
+    BigDecimal montantArticles(Integer numeroCommande);
 }
