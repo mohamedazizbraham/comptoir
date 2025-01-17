f55d1d1cd0b7a5ccc9efe866faf8f78982dac4fb
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2025-01-17 15:15:23 +0100

    changement du nom

---
+++ src/main/java/projection/CommandeProjection.java
@@ -0,0 +1,13 @@
+package projection;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface CommandeProjection {
+    Integer getNumeroCommande();
+    LocalDate getSaisiele();
+    LocalDate getEnvoyeele();
+    BigDecimal getPort();
+    String gedtDestinatiare();
+    BigDecimal getRemise();
+}
